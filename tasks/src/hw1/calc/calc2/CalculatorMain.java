07b0301ac1ce55adb0fe88c38a4fb298cda097c4
alexander <alexace013@gmail.com>
2015-12-10 00:43:44 +0200

    10/12/15 week1.txt, new calc (not until the end)

--- tasks/src/hw1/calc/calc2/CalculatorMain.java
+++ tasks/src/hw1/calc/calc2/CalculatorMain.java
@@ -1,7 +1,5 @@
 package hw1.calc.calc2;
 
-import java.util.Scanner;
-
 public class CalculatorMain {
 
     public static void main(String[] args) {
@@ -9,7 +7,7 @@ public class CalculatorMain {
         AbstractProcessor processor = new Processor();
         Calculator calculator = new Calculator(processor);
 
-        Scanner scan = new Scanner(System.in);
+//        Scanner scan = new Scanner(System.in);
         calculator.inputSymbol('9');
         calculator.inputSymbol('-');
         calculator.inputSymbol('3');
